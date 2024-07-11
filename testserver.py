3ce7cc866bbe5756bfc9051373b3e3c633ecb7ad
Mara Schulze <marschul@2-H-1.42heilbronn.de>
2024-07-11 22:52:20 +0200

    Implemented registration page

--- testserver.py
+++ testserver.py
@@ -2,35 +2,57 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
 import json
 
+def do_login(handler):
+    if handler.path == '/login':
+        content_length = int(handler.headers['Content-Length'])
+        post_data = handler.rfile.read(content_length)
+        data = json.loads(post_data.decode('utf-8'))
+
+        username = data.get('username')
+        password = data.get('password')
+        print(post_data, username, password)
+
+        valid_username = 'user'
+        valid_password = 'pass'
+
+        response = {}
+        if username == valid_username and password == valid_password:
+            print("right")
+            handler.send_response(200)
+            response['message'] = 'Login successful!'
+            response["Set-Cookie"] = "sessionid=foo"
+        else:
+            print("wrong")
+            handler.send_response(401)
+            response['message'] = 'Invalid credentials'
+
+        handler.send_header('Content-Type', 'application/json')
+        handler.end_headers()
+        handler.wfile.write(json.dumps(response).encode('utf-8'))
+        
+def do_register(handler):
+    content_length = int(handler.headers['Content-Length'])
+    post_data = handler.rfile.read(content_length)
+    data = json.loads(post_data.decode('utf-8'))
+    if data["username"] == "existinguser":
+        response = {"error": 1}
+        handler.send_response(400)
+    elif not data["username"].isalnum() or not data["password"].isalnum():
+        response = {"error": 2}
+        handler.send_response(400)
+    else:
+        response = {}
+        handler.send_response(200)
+    handler.send_header('Content-Type', 'application/json')
+    handler.end_headers()
+    handler.wfile.write(json.dumps(response).encode('utf-8'))
+
 class MyHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         if self.path == '/login':
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            data = json.loads(post_data.decode('utf-8'))
-
-            username = data.get('username')
-            password = data.get('password')
-            print(post_data, username, password)
-
-            # Für Testzwecke, verwenden Sie fest codierte Anmeldeinformationen
-            valid_username = 'user'
-            valid_password = 'pass'
-
-            response = {}
-            if username == valid_username and password == valid_password:
-                print("right")
-                self.send_response(200)
-                response['message'] = 'Login successful!'
-                response["Set-Cookie"] = "sessionid=foo"
-            else:
-                print("wrong")
-                self.send_response(401)
-                response['message'] = 'Invalid credentials'
-
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-            self.wfile.write(json.dumps(response).encode('utf-8'))
+            do_login(self)
+        elif self.path == '/register':
+            do_register(self)
         else:
             super().do_POST()
 
