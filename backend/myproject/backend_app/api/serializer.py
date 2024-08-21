be46802c47ffd8761463da311679ce47d625705f
paghe <alessandro.paghera99@gmail.com>
2024-08-21 13:26:14 +0200

    feature/add api for live table match

--- backend/myproject/backend_app/api/serializer.py
+++ backend/myproject/backend_app/api/serializer.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
-from backend_app.models import User 
+from backend_app.models import User, TableMatch
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8, max_length=20)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+    
+    def validate_username(self, value):
+        if not value.isalnum():
+            raise serializers.ValidationError("Username must be alphanumeric.")
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists")
+        return value
+    
+    def create(self, validate_data):
+        user = User(
+            username=validate_data['username'],
+        )
+        user.set_password(validate_data['password'])
+        user.save()
+        return user
+    
+class TableMatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TableMatch
+        fields = '__all__'
+        
--- backend/myproject/backend_setting/settings.py
+++ backend/myproject/backend_setting/settings.py
@@ -143,6 +143,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.backend_setting.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
