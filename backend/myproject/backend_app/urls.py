e33b12781c74fe69a00de955fc68b9c7a273a47c
Lena <lennykulichkova@gmail.com>
2024-10-31 22:09:07 +0100

    fix settings and how-to-play color

--- backend/myproject/backend_app/urls.py
+++ backend/myproject/backend_app/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('', index_view, name='index'), # main page
 	# path('login/', login_view, name='login'), # login
 	# path('registration/', register_view, name='register'), # registration
-	# path('start/', start_view, name='start'),
+	# path('start/', index_view, name='start'),
 	# path('profile/', profile_view, name='profile'), #profile
 ]
