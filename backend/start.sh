f709cd5bab164613070126c935465d80e747e6f1
robxxt <personal.robert@protonmail.com>
2024-11-05 18:01:38 +0100

    made nginx to work

--- backend/start.sh
+++ backend/start.sh
@@ -9,7 +9,7 @@ echo "Creating game_ai user..."
 python myproject/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_user(username='game_ai', email='game_ai@game_ai.game_ai', password='game_ai') if not User.objects.filter(username='game_ai').exists() else None"
 echo "Starting server..."
 python myproject/manage.py runserver 0.0.0.0:8000
-#daphne -b 0.0.0.0 -p 8000 backend_setting.asgi:application
+# daphne -b 0.0.0.0 -p 8000 backend_setting.asgi:application
 
 # sleep 5
 # pytest
